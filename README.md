a479686f4de5e1b03ef4dc67c8bee31d77b20216
Raúl BM <parmesan69@hotmail.com>
2025-01-20 21:38:56 -0600

    Se agrego la funcionalidad de agregar amigos y sortear un número aleatorio para elegir a un ganador del grupo de amigos; incluye documentación en README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proyecto: Amigo Secreto
+
+## Descripción
+Challenge del programa Oracle Next Education - Grupo 8. Este proyecto consiste en una aplicación que permite agregar un grupo de amigos y seleccionar aleatoriamente a uno de ellos. El amigo elegido será el ganador de un juego secreto."
+
+## Características
+- Registro de usuarios
+- Asignación aleatoria de un ganador del grupo de amigos registrados.
+
--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+// Función para agregar un amigo.
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(amigo);
+    }
+    
+    document.getElementById("amigo").value = "";
+
+    // Llamamos la función para actualizar la lista de amigos cada vez que se agrega un amigo nuevo.
+    actualizarListaAmigos()
+};
+
+// Función para actualizar la lista de amigos.
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[index];
+        lista.append(li);
+    }
+};
+
+// Función para elegir un amigo aleatoriamente
+function sortearAmigo() {
+    if (amigos.length <= 1) {
+        alert("Lista de amigos vacía, Agrega mínimo dos amigos para hacer el sorteo.");
+    } else {
+        indiceSorteado = generarIndiceAleatorio();
+        console.log()
+        document.getElementById("resultado").innerHTML = `El amigo secreto sorteado es: ${amigos[indiceSorteado]}`
+    }
+}
+
+// Función para generar un indice aleatorio para seleccionar un amigo de la lista.
+function generarIndiceAleatorio() {
+    return Math.floor(Math.random()*amigos.length)
+}
